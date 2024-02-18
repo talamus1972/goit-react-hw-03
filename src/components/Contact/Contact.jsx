8d8e3ab22557b79f6c29e414b1c113d457cf00d4
talamus1972 <talamus1972@gmail.com>
2024-02-18 23:13:52 +0200

    icon

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,4 +1,6 @@
 import css from "./Contact.module.css"
+import { IoPersonSharp } from "react-icons/io5";
+import { FaPhone } from "react-icons/fa6";
 
 export default function Contact({name, number}) {
     return (
@@ -6,11 +8,11 @@ export default function Contact({name, number}) {
             <li className={css.item} >
                 <div>
                 <div className={css.text} >
-                <span>Icon</span>       
+                <span><IoPersonSharp/></span>       
                 <p>{name}</p>
             </div>  
             <div className={css.text}>
-                <span>Icon</span>           
+                <span><FaPhone/></span>           
                 <p>{number}</p>                
             </div>
                 </div>    
