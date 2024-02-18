d200d0918a47b9da0d08590fe6aace7a7b437f5f
talamus1972 <talamus1972@gmail.com>
2024-02-19 00:06:30 +0200

    2

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,16 +1,19 @@
-// import { useState } from 'react'
+import { useState } from 'react'
 import ContactList from "../ContactList/ContactList"
 import contacts from "../contacts.json"
 import SearchBox from "../SearchBox/SearchBox"
 
 export default function App() {
-  
+  const [inputValue, setInputValue] = useState("");
+  const handleChange = (evt) => {
+    setInputValue(evt.target.value);
+  };
 
 return (
 <div>
     <h1>Phonebook</h1>
   {/* <ContactForm /> */}
-  <SearchBox />
+  <SearchBox value={inputValue} onChange={handleChange}/>
   <ContactList contacts ={contacts} />       
 </div>
   )
--- src/components/SearchBox/SearchBox.jsx
+++ src/components/SearchBox/SearchBox.jsx
@@ -1,10 +1,12 @@
 import css from "./SearchBox.module.css"
 
-export default function SearchBox() {
+export default function SearchBox({ value, onChange }) {
+    console.log(value);
     return (
         <>
             <p>Find contacts by name</p>
             <input type="text" className={css.input} />
+            
         </>
             
     )
