3141d8716d73d1adc8d5a17f7280e998667bd8dc
talamus1972 <talamus1972@gmail.com>
2024-02-19 07:48:19 +0200

    3

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -3,20 +3,42 @@ import ContactList from "../ContactList/ContactList"
 import contacts from "../contacts.json"
 import SearchBox from "../SearchBox/SearchBox"
 
+// export default function App() {
+//   const [inputValue, setInputValue] = useState("");
+//   const handleChange = (evt) => {
+//     setInputValue(evt.target.value);
+//   };
+
+// return (
+// <div>
+//     <h1>Phonebook</h1>
+//   {/* <ContactForm /> */}
+//   <SearchBox value={inputValue} onChange={handleChange}/>
+//   <ContactList contacts ={contacts} />       
+// </div>
+//   )
+// }
+
 export default function App() {
-  const [inputValue, setInputValue] = useState("");
+  const [inputValue, setInputValue] = useState('');
+  const [filteredContacts, setFilteredContacts] = useState(contacts);
+
   const handleChange = (evt) => {
-    setInputValue(evt.target.value);
+    const searchTerm = evt.target.value;
+    setInputValue(searchTerm);
+
+    const filtered = contacts.filter((contact) =>
+      contact.name.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+
+    setFilteredContacts(filtered);
   };
 
-return (
-<div>
-    <h1>Phonebook</h1>
-  {/* <ContactForm /> */}
-  <SearchBox value={inputValue} onChange={handleChange}/>
-  <ContactList contacts ={contacts} />       
-</div>
-  )
+  return (
+    <div>
+      <h1>Phonebook</h1>
+      <SearchBox value={inputValue} onChange={handleChange} />
+      <ContactList contacts={filteredContacts} />
+    </div>
+  );
 }
-
-
--- src/components/SearchBox/SearchBox.jsx
+++ src/components/SearchBox/SearchBox.jsx
@@ -1,11 +1,11 @@
 import css from "./SearchBox.module.css"
 
 export default function SearchBox({ value, onChange }) {
-    console.log(value);
+    
     return (
         <>
             <p>Find contacts by name</p>
-            <input type="text" className={css.input} />
+            <input type="text" className={css.input} onChange={onChange} value={value} />
             
         </>
             
